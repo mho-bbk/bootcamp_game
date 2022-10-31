774790cfa6963d632a803b977feedbbb4fe8d623
User <kmoyinho@gmail.com>
2022-10-31 17:09:00 +0000

    Added GUI feature to Run

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,9 +1,21 @@
 
 import java.util.ArrayList;
+
 import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Run {
 
+	JFrame window = new JFrame("Treasure Hunt");
 	Scanner inp = new Scanner(System.in);
 
     Enemy enemy;
@@ -34,6 +46,7 @@ public class Run {
 	        GridController newGrid = new GridController(gridSizeAsInt);
 	        // GENERATE TREASURE
 	        TREASURE = new Treasure(gridSizeAsInt);
+	        
 	        //TODO
 	        // GENERATE ENEMIES
 	        enemy = new Enemy("Gerard", gridSizeAsInt);
@@ -52,6 +65,7 @@ public class Run {
 	        // CHECK PLAYER DOESN'T SPAWN IN SAME SQ AS TREASURE
 	        if(TREASURE.checkWin(p)) {
 	        	System.out.println(winMessage);
+	        	winGame(p.getName());
 	        	finished = true;
 	        } else if (enemy.getCurrentPosition().equals(p.getCurrentPosition())) {
 	        	// CHECK PLAYER DOESN'T SPAWN IN SAME SQ AS ENEMY
@@ -61,6 +75,7 @@ public class Run {
 	        	System.out.println("The treasure is somewhere...");
 	        }
 	        
+	        init_window(newGrid, gridSizeAsInt);
 	        
 	        // BEGIN PLAYER MOVES
 	        while(!finished) {
@@ -76,17 +91,23 @@ public class Run {
 			        case "X": finished = true; break;
 		        }
 		        
-		        newGrid.setPosition(p); 
-		        newGrid.printGrid();
+		        newGrid.setPosition(p);
+		        // Initiate GUI
+		        init_window(newGrid, gridSizeAsInt);
+//		        newGrid.printGrid();
 		        
-		        //Check treasure
+		        //Don't check if just exiting
 		        if(!move.toUpperCase().equals("X")) {
+		        	//Check treasure
 			        if(TREASURE.checkWin(p)) {
 			        	System.out.println(winMessage);
+			        	winGame(p.getName());
 			        	finished = true;
 			        } else {
 			        	System.out.println(TREASURE.compareDistanceFromPrevious(p));
 			        }
+			        
+			        //TODO - check enemy
 		        }
 	        }
 	        
@@ -102,11 +123,67 @@ public class Run {
     
     // input handler
     private String getInput(String s) {
-        System.out.println(s);
+        System.out.print(s);
         //TODO - input validation
         String ans = inp.next();
         return ans;
         
+    }
+    
+    public void winGame(String name) {
+        window.getContentPane().removeAll();
+        window.repaint();
+        JLabel win = new JLabel();
+        win.setText(name + " won. I love cheeseburgers.");
+        window.add(win);
+        window.pack();
+        window.setVisible(true);
+    }
+
+   // GUI Handler
+    public void init_window(GridController grid, int gridSize) {
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new GridLayout(gridSize, gridSize));
+        window.setLocationRelativeTo(null);
+        window.getContentPane().removeAll();
+        window.repaint();
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                JLabel label = addEntity(grid.grid[i][j]);
+                window.add(label);
+            }
+        }
+        window.pack();
+        window.setVisible(true);
+    }
+
+
+    public void update(GridController grid, int gridSize) {
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                JLabel label = addEntity(grid.grid[i][j]);
+                window.add(label);
+            }
+        }
+        window.repaint();
+    }
+
+
+
+   public JLabel addEntity(char c) {
+        JLabel label = new JLabel();
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setPreferredSize(new Dimension(50, 50));
+        switch (c) {
+        case '•':
+            label.setBackground(Color.CYAN);
+            break;
+        case 'P':
+            label.setBackground(Color.black);
+        }
+        label.setOpaque(true);
+        label.setBorder(BorderFactory.createLineBorder(Color.WHITE, 1));
+        return label;
     }
 
 }
